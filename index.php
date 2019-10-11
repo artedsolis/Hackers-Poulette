90e71c85b15c18b1db4aa38690730f6088677b06
arturo <artedsolis@gmail.com>
2019-10-11 14:00:36 +0200

    commiting finals

--- index.php
+++ index.php
@@ -40,8 +40,8 @@ if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['gend
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Hackers Poulette Contact Website</title>
-  <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
